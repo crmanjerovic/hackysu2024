f506a7f969179b6621d677e95bf38401c72ee22a
keith manjerovic <crmanjerovic@gmail.com>
2024-04-06 15:47:14 -0400

    Created Tile class

--- Assets/waveFunctionCollapse.cs
+++ Assets/waveFunctionCollapse.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class waveFunctionCollapse : MonoBehaviour
+public class WaveFunctionCollapse : MonoBehaviour
 {
+
     public const int NUM_TILES = 3;
-    public const int TILE_SIZE = 1;
+    public const float TILE_SIZE = 2f;
     public const int MAP_SIZE = 30;
 
     public GameObject[] tiles = new GameObject[NUM_TILES]; //add as many tiles as you want
     string[] tileNames = new string[NUM_TILES];
 
     //public const string[][] RULESET = new string[][];
-
-    int i = 0;
 
     GameObject[,] map = new GameObject[MAP_SIZE, MAP_SIZE];
 
@@ -23,20 +22,21 @@ public class waveFunctionCollapse : MonoBehaviour
         for (int i=0; i < NUM_TILES; i++) //have an array of tiles names for readability
         {
             tileNames[i] = tiles[i].name;
-            Debug.Log(tileNames[i]);
         }
+
+        // for (int i = 0; i < MAP_SIZE; i++)
+        // {
+        //     for (int j = 0; j < MAP_SIZE; j++)
+        //     {
+        //         map[i, j] = tiles[Random.Range(0, NUM_TILES)];
+        //         Instantiate(map[i, j], new Vector3(TILE_SIZE * i, 0, TILE_SIZE * j), Quaternion.Euler(new Vector3(-90, 0, 0)));
+        //     }
+        // }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (i < MAP_SIZE) { 
-            for (int i2 = 0; i2 < MAP_SIZE; i2++)
-            {
-                map[i, i2] = tiles[Random.Range(0, 3)];
-                Instantiate(map[i, i2], new Vector3(TILE_SIZE * i, 0, TILE_SIZE * i2), Quaternion.Euler(new Vector3(-90, 0, 0)));
-            }
-            i++;
-        }
+      
     }
 }
