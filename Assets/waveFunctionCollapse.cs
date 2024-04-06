2d64fcab7a9ff85536489da071d0112327d0f7f3
keith manjerovic <crmanjerovic@gmail.com>
2024-04-06 12:28:17 -0400

    Reformatted tiles folder  - Removed tile prefabs  + Added 3 sample fbx tiles

--- Assets/waveFunctionCollapse.cs
+++ Assets/waveFunctionCollapse.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class waveFunctionCollapse : MonoBehaviour
 {
-    public const int NUM_TILES = 20;
+    public const int NUM_TILES = 3;
     public const int TILE_SIZE = 1;
     public const int MAP_SIZE = 30;
 
@@ -33,7 +33,7 @@ public class waveFunctionCollapse : MonoBehaviour
         if (i < MAP_SIZE) { 
             for (int i2 = 0; i2 < MAP_SIZE; i2++)
             {
-                map[i, i2] = tiles[Random.Range(0, 2)];
+                map[i, i2] = tiles[Random.Range(0, 3)];
                 Instantiate(map[i, i2], new Vector3(TILE_SIZE * i, 0, TILE_SIZE * i2), Quaternion.Euler(new Vector3(-90, 0, 0)));
             }
             i++;
