e21e47f15be5e942e5a4f831dcda2803638e8957
cgmanjerovic <cgmanjerovic@student.ysu.edu>
2024-04-05 23:53:40 -0400

    First commit, project skeleton and .gitignore

---
+++ Assets/waveFunctionCollapse.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waveFunctionCollapse : MonoBehaviour
+{
+    public GameObject[] tiles = new GameObject[20]; //add as many tiles as you want
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
